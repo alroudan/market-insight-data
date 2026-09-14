3f4e38e09ddb1c70c2f60df3d0ed4bcf3c66e3a8
alroudan <88317628+alroudan@users.noreply.github.com>
2026-09-14 09:16:14 +0300

    Refresh clean app document cache

--- website/sw.js
+++ website/sw.js
@@ -1,4 +1,4 @@
-const CACHE='market-insight-v4';
+const CACHE='market-insight-v5';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
